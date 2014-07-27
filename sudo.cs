4e53e57b5c034fedb4c0d081b11eccb2cbc793db
cd01 <ibdcd01@Gmail.com>
2014-07-27 15:17:06 +0900

    Add version information

--- sudo.cs
+++ sudo.cs
@@ -2,33 +2,35 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Reflection;
 
-namespace sudo
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyCopyright("Copyright cd01 2014")]
+[assembly: AssemblyDescription("Elevate UAC")]
+class sudo
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            var startInfo = new System.Diagnostics.ProcessStartInfo()
-                            {
-                                FileName = args[0],
-                                UseShellExecute = true,
-                                Verb = "runas",
-                                Arguments = new Func<IEnumerable<string>, string>((argsInShell) =>
-                                            {
-                                                return string.Join(" ", argsInShell);
-                                            })(args.Skip(1))
-                            };
- 
-            try 
-            { 
-                var proc = System.Diagnostics.Process.Start(startInfo);
-                proc.WaitForExit();
-            } 
-            catch(Exception ex) 
-            { 
-                Console.Error.WriteLine(ex.Message); 
-            } 
-        }
+        var startInfo = new System.Diagnostics.ProcessStartInfo()
+                        {
+                            FileName = args[0],
+                            UseShellExecute = true,
+                            Verb = "runas",
+                            Arguments = new Func<IEnumerable<string>, string>((argsInShell) =>
+                                        {
+                                            return string.Join(" ", argsInShell);
+                                        })(args.Skip(1))
+                        };
+
+        try 
+        { 
+            var proc = System.Diagnostics.Process.Start(startInfo);
+            proc.WaitForExit();
+        } 
+        catch(Exception ex) 
+        { 
+            Console.Error.WriteLine(ex.Message); 
+        } 
     }
 }
+
